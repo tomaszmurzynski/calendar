46e602b4bffd85aa29e5c5f7903bfec124b83e64
Tomasz Murzynski <tomasz.murzynski@gmail.com>
2023-12-07 11:29:31 +0100

    edit event

--- update.php
+++ update.php
@@ -4,7 +4,7 @@ if(isset($_POST["id"]))
 {
  $query = "
  UPDATE event 
- SET title=:title, start_event=:start_event, end_event=:end_event 
+ SET title=:title, start_event=:start_event, end_event=:end_event, desc_event=:desc_event, place_event=:place_event
  WHERE id=:id
  ";
  $statement = $connect->prepare($query);
@@ -13,7 +13,9 @@ if(isset($_POST["id"]))
    ':title'  => $_POST['title'],
    ':start_event' => $_POST['start'],
    ':end_event' => $_POST['end'],
-   ':id'   => $_POST['id']
+   ':id'   => $_POST['id'],
+   ':place_event' => $_POST['place'],
+   ':desc_event' => $_POST['desc']
   )
  );
 }
